a29e74a78ba06c9b4b48e634600fffd2f2e98e8c
GalAster <280597828@qq.com>
2017-08-06 23:21:53 +0800

    [Functions] Initial

---
+++ !Functions.m
@@ -0,0 +1,22 @@
+(* Mathematica Package *)
+(* Created by Mathematica Plugin for IntelliJ IDEA *)
+
+(* :Title: !Functions *)
+(* :Context: !Functions` *)
+(* :Author: 28059 *)
+(* :Date: 2017-08-06 *)
+
+(* :Package Version: 0.1 *)
+(* :Mathematica Version: *)
+(* :Copyright: (c) 2017 28059 *)
+(* :Keywords: *)
+(* :Discussion: *)
+
+BeginPackage["!Functions`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"]
+
+End[] (* `Private` *)
+
+EndPackage[]
